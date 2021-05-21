166b3af9869dfb37796e7ad13f9255368ae320f8
nikul <dankharanikul@gmail.com>
2021-05-21 17:01:22 +0530

    v1.0.0
    frist

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-layout view="lHh Lpr fff">
+
+    <q-header reveal elevated class="bg-grey-3 text-dark" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          
+          <img src="~assets/logo.png" width="128">
+        </q-toolbar-title>
+        <q-btn flat round dense icon="search" />
+        <q-btn dense flat round icon="menu" @click="right = !right" />
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer v-model="right" side="right" overlay behavior="mobile" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer reveal elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+            <img src="~assets/logo.png" width="128">
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      right: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,120 @@
+<template>
+  <q-page>
+    <div class="q-pa-md">
+      <q-carousel
+        animated
+        v-model="slide"
+        navigation
+        infinite
+        :autoplay="autoplay"
+        arrows
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        @mouseenter="autoplay = false"
+        @mouseleave="autoplay = true"
+      >
+        <q-carousel-slide
+          :name="1"
+          img-src="https://cdn.quasar.dev/img/mountains.jpg"
+        />
+        <q-carousel-slide
+          :name="2"
+          img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+        />
+        <q-carousel-slide
+          :name="3"
+          img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+        />
+        <q-carousel-slide
+          :name="4"
+          img-src="https://cdn.quasar.dev/img/quasar.jpg"
+        />
+      </q-carousel>
+    </div>
+    <div class="q-pa-md row items-start q-gutter-md cards">
+      <q-card class="my-card ">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none"> </q-card-section>
+      </q-card>
+      <q-card class="my-card ">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none"> </q-card-section>
+      </q-card>
+      <q-card class="my-card ">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none"> </q-card-section>
+      </q-card>
+      <q-card class="my-card ">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none"> </q-card-section>
+      </q-card>
+      <q-card class="my-card ">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none"> </q-card-section>
+      </q-card>
+    </div>
+    <div class="row">
+      <q-banner rounded class="bg-grey-3 my-card">
+      <template v-slot:avatar>
+        <img
+          src="https://cdn.quasar.dev/img/mountains.jpg"
+          style="height: 300px"
+        >
+      </template>
+
+      
+    </q-banner>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      slide: 1,
+      autoplay: true
+    };
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+
+  max-width: 250px
+.cards
+  background-color: $cyan-13
+</style>
